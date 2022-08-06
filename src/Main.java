a62bebe2517a877f92293d1477146b5bf39fe18b
veronica <veronica.bmazza@gmail.com>
2022-08-06 17:44:31 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        Curso curso2 = new Curso();
+
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descricao curso js");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+
+        mentoria1.setTitulo("mentoria java");
+        mentoria1.setDescricao("descricao mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
+
+
+    }
+}
